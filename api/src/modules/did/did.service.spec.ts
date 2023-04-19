ebf8774b509a643245bb3eed5acc8a82acd42059
matucasal <matias.casal@yahoo.com.ar>
2023-04-19 18:12:33 +0200

    fixing tests

--- api/src/modules/did/did.service.spec.ts
+++ api/src/modules/did/did.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DidService } from './did.service';
+import { IpfsService } from '../../providers/ipfs/ipfs.service';
 
 describe('DidService', () => {
   let service: DidService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DidService],
+      providers: [DidService, IpfsService],
     }).compile();
 
     service = module.get<DidService>(DidService);
